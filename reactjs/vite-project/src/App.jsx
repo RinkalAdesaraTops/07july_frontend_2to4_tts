db76deb2f4596d9f34e4624740fe9729ac4fd39a
Rinkal <rinkaladesara.tops@gmail.com>
2025-11-07 13:03:55 +0530

    ok

--- reactjs/vite-project/src/App.jsx
+++ reactjs/vite-project/src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 import ClassExample from './ClassExample'
 import LifecycleFunction from './LifecycleFunction'
+import Usercomponent from './Usercomponent'
 
 function App() {
   return (
     <>
-      <h3>LIfecycle Example</h3>
-      <ClassExample myname="test" />
+      {/* <h3>LIfecycle Example</h3> */}
+      {/* <ClassExample myname="test" /> */}
       {/* <LifecycleFunction /> */}
+      <Usercomponent />
     </>
   )
 }
---
+++ reactjs/vite-project/src/Usercomponent.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Form, Button, Table } from "react-bootstrap";
+const Usercomponent = () => {
+    const [name,setName] = useState('')
+    const [age,setAge] = useState('')
+    const [salary,setSalary] = useState('')
+    const [data,setData] = useState([])
+    let handleAge = (e)=>{
+        setAge(e.target.value)
+    }
+    const saveData = (e)=>{
+        e.preventDefault()
+        setData([
+            ...data,
+            {name,age,salary}
+            // {
+            //     "name":name,
+            //     "age":age,
+            //     "salary":salary
+            // }
+        ])
+        setName('')
+        setAge('')
+        setSalary('')
+       }
+  return (
+    <div>
+      <h3>User Registration Form</h3>
+      <Container fluid="md">
+        <Row>
+          <Col>
+            <Form name="frm" method="post" action="#" onSubmit={saveData}>
+              <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" placeholder="Enter name" name="name" onChange={(e)=>setName(e.target.value)} value={name}/>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formGroupPassword">
+                <Form.Label>Age</Form.Label>
+                <Form.Control type="number" placeholder="Age" name="age" onChange={handleAge} value={age}/>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formGroupsal">
+                <Form.Label>Salary</Form.Label>
+                <Form.Control type="number" placeholder="Salary" name="salary" onChange={(e)=>setSalary(e.target.value)} value={salary}/>
+              </Form.Group>
+            
+            <Button variant="primary" type="submit">
+              Save
+            </Button>
+            <Button variant="danger" type="reset">
+              Cancel
+            </Button>
+            </Form>
+          </Col>
+          <Col>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>User Name</th>
+                  <th>Age</th>
+                  <th>Salary</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                    data.map((i,index)=>{
+                        return (
+                            <tr>
+                                <td>{index+1}</td>
+                                <td>{i.name}</td>
+                                <td>{i.age}</td>
+                                <td>{i.salary}</td>
+                                <td>
+                                    <Button variant="primary" type="button">
+              Edit
+            </Button>
+                                    <Button variant="danger" type="button">
+              Delete
+            </Button>
+                                </td>
+                            </tr>
+                        )
+                    })
+                }
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Usercomponent;
