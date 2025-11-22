f68d9df3acce13302f074413bd07e5dec75e396c
Rinkal <rinkaladesara.tops@gmail.com>
2025-11-22 11:32:24 +0530

    routing

--- reactjs/Apiexample/src/App.jsx
+++ reactjs/Apiexample/src/App.jsx
@@ -3,11 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 import CrudExample from './CrudExample'
+import Menu from './Menu'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import About from './About'
+import Home from './Home'
+import Contact from './Contact'
 
 function App() {
   return (
     <>
-    <CrudExample />
+    <Menu />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/home' element={<Home />}/>
+      <Route path='/about' element={<About />}/>
+      <Route path='/contact' element={<Contact />}/>
+    </Routes>
+    </BrowserRouter>
+    {/* <CrudExample /> */}
     </>
   )
 }
