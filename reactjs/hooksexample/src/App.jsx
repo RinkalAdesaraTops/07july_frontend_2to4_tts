fc506dbb47ceca68b12f27cddbbfed0537163c5c
Rinkal <rinkaladesara.tops@gmail.com>
2025-11-27 13:02:00 +0530

    useRef & UseCOntext Hooks

---
+++ reactjs/hooksexample/src/App.jsx
@@ -0,0 +1,18 @@
+
+import ContextExample from './ContextExample'
+import Home from './Home'
+import UseContextExample from './UseContextExample'
+import UseRefExample from './UseRefExample'
+
+function App() {
+  return (
+    <>
+    {/* <Home /> */}
+    {/* <UseRefExample /> */}
+    {/* <ContextExample /> */}
+    <UseContextExample />
+    </>
+  )
+}
+
+export default App
---
+++ reactjs/hooksexample/src/Home.jsx
@@ -0,0 +1,20 @@
+import { useRef, useState } from "react";
+const Home = () => {
+  let [count,setCount] = useState(0)
+  let a=useRef(10)
+  const addCounter = ()=>{
+      setCount(count+1)
+      a.current += 1
+      console.log(a);   
+    }
+  return (
+    <>
+      <h3>Home component Called..</h3>
+      <h4>Use Ref Hooks example</h4>
+      <h4>Count is -- {count}</h4>
+      <h4>A is -- {a.current}</h4>
+      <button onClick={addCounter}>Add Counter</button>
+    </>
+  );
+};
+export default Home;
